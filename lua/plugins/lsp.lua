dd12f0ef120e861af3806ab18b4edabc36ed16b0
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-02-11 11:36:49 +0100

    batman

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,69 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "pyright",
+                    "tsserver",
+                    "clangd",
+                },
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                on_init = function(client)
+                    local path = client.workspace_folders[1].name
+                    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+                        client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                            Lua = {
+                                runtime = {
+                                    version = 'LuaJIT'
+                                },
+                                workspace = {
+                                    checkThirdParty = false,
+                                    library = {
+                                        vim.env.VIMRUNTIME
+                                    }
+                                }
+                            }
+                        })
+                        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+                        end
+                    return true
+                end
+            })
+            lspconfig.pyright.setup {
+                capabilities = capabilities,
+            }
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.dockerls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.docker_compose_language_service.setup({
+                capabilities = capabilities,
+            })
+        end
+    }
+}
