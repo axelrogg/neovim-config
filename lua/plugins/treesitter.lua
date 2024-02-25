d157fa0f5a9577578dd01f0018ddd9a223f7da69
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-02-25 07:42:48 +0100

    Add nvim-ts-autotag for automatically closing tsx html elements

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,31 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            ensure_installed = {
-                "c",
-                "lua",
-                "javascript",
-                "typescript",
-                "python",
-                "go"
-            },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+    {
+        "windwp/nvim-ts-autotag",
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                ensure_installed = {
+                    "c",
+                    "go",
+                    "javascript",
+                    "lua",
+                    "python",
+                    "typescript",
+                    "tsx",
+                },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+                -- enable autoclose html tags thanks to nvim-ts-autotag
+                autotag = {
+                    enable = true,
+                },
+            })
+        end
+
+    }
 }
